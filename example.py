0fc0fa96c48783d3729e24e0b7f7ef30bdc08a37
Adam Christensen <adam.christensen@gmail.com>
2020-07-06 21:10:47 -0400

    bug fix with gsp tables in windc_2_0_1

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     w1 = wd.WindcEnvironment(
         gams_sysdir="/Applications/GAMS30.3/Resources/sysdir",
         data_dir="~/Desktop/windc_raw_data",
-        version="windc_2_1",
+        version="windc_2_0_1",
     )
 
     w1.rebuild(gdxout=True)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="windc_data",
-    version="0.1.6",
+    version="0.1.7",
     author="Adam Christensen",
     author_email="achristensen@gams.com",
     description="A helper package to read/write WiNDC Data",
