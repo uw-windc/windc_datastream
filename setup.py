2e81ad88face45218f04bfe04cf4083b9bd17329
Adam Christensen <adam.christensen@gmail.com>
2020-07-10 07:58:56 -0400

    version number increment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="windc_data",
-    version="0.1.11",
+    version="0.1.12",
     author="Adam Christensen",
     author_email="achristensen@gams.com",
     description="A helper package to read/write WiNDC Data",
